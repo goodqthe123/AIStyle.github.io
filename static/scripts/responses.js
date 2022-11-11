7cad41d36a2cc7b2ca01a9243d83720e8ad5cd81
goodqthe123 <111725853+goodqthe123@users.noreply.github.com>
2022-11-11 14:09:36 +0800

    Add files via upload

--- static/scripts/responses.js
+++ static/scripts/responses.js
@@ -1,12 +1,16 @@
-var favoritefood = "";
-var live ="";
+
 function getBotResponse(input){
   
+    var favoritefood = "";
+    var live ="";
+    var name ="";
 
 
+    if (input.includes('name')) {
+        let name =  input.substr(10);  //My name is 
+        console.log(name);
+        return `"Morning ${name}, I know a few friends named ${name} who are very friendly, easy-going and easy to get along with, aren't you? :) "`;
 
-    if (input == "Marry" || input == "Kiana" || input == "Leslie" || input == "Carry") {
-        return `"Morning ${input}, I know a few female friends named ${input} who are very friendly, easy-going and easy to get along with, aren't you? :) "`;
     } else if (input == "Yes" || input == "Somtime" ) {
         return "Haha, I guess you like desserts, right?";
     } else if (input == "Yes I like desserts" || input == "Yes I am" || input == "Yes, I like desserts") {
@@ -17,23 +21,28 @@ function getBotResponse(input){
             return "oic, I love desserts too! I live in Kwun Tong ,and you??"; 
 
           }
-    } else if (input == "No i am not" || input == "No I don't like desserts" ) {
+    } else if (input == "No" || input == "No I don't like desserts" ) {
           
-         if(input == ""){
+         if(input == "No" || input == "No I don't like desserts" ){
             return "Oic ,What is your favorite food";
-            let favoritefood =  input;
+           
          }
        
                //bugs
           
-    }  else if (input.includes('live') ) {       //input == "I live in Kwun Tong Too"
-         let live =  input.substr(10);   // I live in 
-         console.log(live);
+    }  else if (input.includes('favorite food') ) {       //input == "My favorite food is"
+         let favoritefood =  input.substr(19);   // I live in 
+         console.log(favoritefood);
 
-        return `"I will pass by ${live} after work on Thursdays and Fridays. I know there is a ${favoritefood} dessert shop. When are you free ?Thursdays Or Fridays?"`;
-    }  else if (input == "Thursdays" ) {
+        return `"OMG ,I love ${favoritefood} too ,I think this is the most delicious food in the world!!! I live in Kwun Tong ,and you??"`;
+    }  else if (input.includes('live') ) {       //input == "I live in Kwun Tong Too"
+        let live =  input.substr(10);   // I live in 
+        console.log(live);
+
+       return `"I will pass by ${live} after work on Thursdays and Fridays. I know there is a ${favoritefood} dessert shop. When are you free ?Thursdays Or Fridays?"`;
+   } else if (input == "Thursday" ) {
         return "Great, see you on Thursdays night";
-    } else if (input == "Fridays" ) {
+    } else if (input == "Friday" ) {
         return "Great, see you on Fridays night";
     }     else if (input == "rock") {  
         return "paper";
